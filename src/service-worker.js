480ccd3ae2d801d6e6e6b09e556f6e9361791805
anasanshu <anasanshu68@gmail.com>
2020-08-10 11:44:22 +0000

    Remove workbox

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,8 +1,1 @@
 importScripts('ngsw-worker.js');
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-if (workbox) {
-  console.log(`Yay! Workbox is loaded 🎉`);
-} else {
-  console.log(`Boo! Workbox didn't load 😬`);
-}
